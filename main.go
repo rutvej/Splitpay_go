c969c04577ae782ec220ebfc706e2495c9cd4fc3
abc <rutvej.waghela@routemobile.com>
2022-06-26 13:46:42 +0530

    ADDING port

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 		"net/http"
 		"log"
 		"github.com/gorilla/mux"
+		"os"
 )
 
 
 func main()  {
+	port := os.Getenv("PORT")
 	router := mux.NewRouter()
 	InitialMigration()
 	router.HandleFunc("/",Home).Methods("GET")
 	router.HandleFunc("/splitbill",CreateTranscation).Methods("POST")
 	router.HandleFunc("/showbill/{number}/{intent}",ShowTranscation).Methods("GET")
 	router.HandleFunc("/pay/{number}/{id}",PayTranscation).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":10021", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
